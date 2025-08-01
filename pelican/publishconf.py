6bc15b758b6142ce27be894d71b4c708eb46add9
petr-plihal <petaplihal@seznam.cz>
2025-08-01 23:52:26 +0200

    ops: add settings for github actions deployment

--- pelican/publishconf.py
+++ pelican/publishconf.py
@@ -8,8 +8,11 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = ""
+SITEURL = "https://petr-plihal.eu"
 RELATIVE_URLS = False
+
+# Override output path for GitHub Actions deployment
+OUTPUT_PATH = "output"
 
 FEED_ALL_ATOM = "feeds/all.atom.xml"
 CATEGORY_FEED_ATOM = "feeds/{slug}.atom.xml"
