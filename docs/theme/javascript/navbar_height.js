fd54beca884a8cd8daa196b3b4eb927bda3ef3bb
petr-plihal <petaplihal@seznam.cz>
2025-08-01 16:06:30 +0200

    chore: remove generated files

--- docs/theme/javascript/navbar_height.js
+++
@@ -1,8 +0,0 @@
-// Get real navbar height and set property to use it for page padding
-document.addEventListener('DOMContentLoaded', function() {
-    const navbar = document.getElementById('mainNavbar');
-    const navbarHeight = navbar.offsetHeight;
-    
-    // Set CSS custom property with the actual navbar height
-    document.documentElement.style.setProperty('--navbar-height', navbarHeight + 'px');
-});
