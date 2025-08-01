35079c6ad9229bb65f64b4de20cd6678511dab40
petr-plihal <petaplihal@seznam.cz>
2025-08-02 00:39:45 +0200

    feat: limit analytics to deployments, add additional tracking info
    The cv downloads and clicked contacts should be tracked now.

--- pelican/publishconf.py
+++ pelican/publishconf.py
@@ -19,7 +19,5 @@ CATEGORY_FEED_ATOM = "feeds/{slug}.atom.xml"
 
 DELETE_OUTPUT_DIRECTORY = True
 
-# Following items are often useful when publishing
-
-# DISQUS_SITENAME = ""
-# GOOGLE_ANALYTICS = ""
+# Custom GA4 implementation
+GOOGLE_ANALYTICS_ID = "G-QWF3EXGP99"
